b38553e3e41ba378f672e4908ed84c3828a42b3d
Captaintoy <abdulsalamkorede64@gmail.com>
2026-01-01 17:48:57 -0800

    wallet

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.middleware.js';
 import * as ordersCtrl from '../controller/orders.controller.js';
-import * as walletCtrl from '../controller/wallet.controller.js';
 import * as historyCtrl from '../controller/history.controller.js';
 import * as usersCtrl from '../controller/users.controller.js';
 
@@ -12,9 +11,7 @@ router.post('/orders', verifyToken, ordersCtrl.createOrder);
 router.get('/orders', verifyToken, ordersCtrl.getOrders);
 router.delete('/orders/:id', verifyToken, ordersCtrl.deleteOrder);
 
-// Wallet
-router.get('/wallet', verifyToken, walletCtrl.getWallet);
-router.post('/wallet/transaction', verifyToken, walletCtrl.createTransaction);
+// Wallet routes removed (wallet functionality deprecated)
 
 // History
 router.get('/history', verifyToken, historyCtrl.getHistory);
